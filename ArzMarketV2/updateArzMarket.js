d71d338b451136532ce0205c4fc1e6acbb31bdb2
FREYM1337 <nikita.meshkov.85@gmail.com>
2025-09-05 14:24:29 +0300

    Update updateArzMarket.js

--- ArzMarketV2/updateArzMarket.js
+++ ArzMarketV2/updateArzMarket.js
@@ -1,4 +1,4 @@
 {
-    "updateurl": "https://raw.githubusercontent.com/FREYM1337/forumnick/main/ArzMarketV3/ArzMarket.lua",
+    "updateurl": "https://raw.githubusercontent.com/FREYM1337/forumnick/main/ArzMarketV2/ArzMarket.lua",
     "latest": "3.00"
 }
