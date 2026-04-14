4b074281ab918adad2c920e80b6022ad3bd2d52f
FREYM1337 <nikita.meshkov.85@gmail.com>
2026-04-14 04:01:02 +0300

    fixes #2

--- ArzMarketV3/updateArzMarket.js
+++ ArzMarketV3/updateArzMarket.js
@@ -1,5 +1,5 @@
 {
     "updateurl": "https://raw.githubusercontent.com/FREYM1337/forumnick/main/ArzMarketV3/ArzMarket.lua",
-    "latest": "3.50",
-    "itemsUpdate": 21
+    "latest": "3.51",
+    "itemsUpdate": 22
 }
